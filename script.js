dfcc108e1245f70135296620100c3eec4bdbf71a
Kajal Kumari <114283323+kajalkumari2507@users.noreply.github.com>
2023-05-13 08:13:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
 /** @format */
 
-let arr = [
-  { id: 1, name: "john", age: "18", marks: 80 },
-  { id: 2, name: "jack", age: "20", marks: 85 },
-  { id: 3, name: "karen", age: "19", marks: 35 },
-];
+let arr = [ 
+            { id: 1, name: "john", age: "18", marks: 80 },    
+            { id: 2, name: "jack", age: "20", marks: 85 },    
+            { id: 3, name: "karen", age: "19", marks: 35 }
+          ];
 
-function PrintStudentswithMap() {
-  //Write your code here , just console.log
+function PrintStudentswithMap() 
+{
+    arr.map(student => 
+    {
+        if (student.marks > 50) 
+        {
+            console.log(student);
+        }
+    });
 }
 
-function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
+function PrintStudentsbyForEach() 
+{
+    arr.forEach(student => 
+    {
+        if (student.marks > 50) 
+        {
+            console.log(student);
+        }
+    });
 }
 
-function addData() {
-  //Write your code here, just console.log
+function addData() 
+{
+    let newStudent = { id: 4, name: "susan", age: "20", marks: 45 };
+    arr.push(newStudent);
+    console.log(newStudent);
 }
 
-function removeFailedStudent() {
-  //Write your code here, just console.log
+function removeFailedStudent() 
+{
+    arr = arr.filter(student => student.marks >= 50);
+    console.log(arr);
 }
 
-function concatenateArray() {
-  //Write your code here, just console.log
+function concatenateArray() 
+{
+    let newArray =  [        
+                        { id: 4, name: "kevin", age: "18", marks: 75 },        
+                        { id: 5, name: "lisa", age: "20", marks: 95 },        
+                        { id: 6, name: "matt", age: "19", marks: 60 }    
+                    ];
+    arr = arr.concat(newArray);
+    console.log(arr);
 }
