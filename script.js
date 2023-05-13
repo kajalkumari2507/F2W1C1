33d457cac2366ae915c377e9596cc7906a685857
Kajal Kumari <114283323+kajalkumari2507@users.noreply.github.com>
2023-05-13 20:31:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,42 @@
 /** @format */
 
-let arr = [ 
-            { id: 1, name: "john", age: "18", marks: 80 },    
-            { id: 2, name: "jack", age: "20", marks: 85 },    
-            { id: 3, name: "karen", age: "19", marks: 35 }
-          ];
+let arr = [
+  { id: 1, name: "john", age: "18", marks: 80 },
+  { id: 2, name: "jack", age: "20", marks: 85 },
+  { id: 3, name: "karen", age: "19", marks: 35 },
+];
 
-function PrintStudentswithMap() 
-{
-    arr.map(student => 
-    {
-        if (student.marks > 50) 
-        {
-            console.log(student);
-        }
-    });
+function PrintStudentswithMap() {
+  //Write your code here , just console.log
+   console.log(arr.filter(arr => arr.marks > 50).map(arr => arr.name));
 }
 
-function PrintStudentsbyForEach() 
-{
-    arr.forEach(student => 
-    {
-        if (student.marks > 50) 
-        {
-            console.log(student);
-        }
-    });
+function PrintStudentsbyForEach() {
+  //Write your code here , just console.log
+  arr.forEach(arr => {
+    if (arr.marks > 50) console.log(arr.name);
+  });
 }
 
-function addData() 
-{
-    let newStudent = { id: 4, name: "susan", age: "20", marks: 45 };
-    arr.push(newStudent);
-    console.log(newStudent);
+function addData() {
+  //Write your code here, just console.log
+  const newStudent = {id:arr.length+1, name:"susan", age:"20", marks:45};
+  arr.push(newStudent);
+  console.log(arr);
 }
 
-function removeFailedStudent() 
-{
-    arr = arr.filter(student => student.marks >= 50);
-    console.log(arr);
+function removeFailedStudent() {
+  //Write your code here, just console.log
+  arr = arr.filter(arr => arr.marks >= 50);
+  console.log(arr);
 }
 
-function concatenateArray() 
-{
-    let newArray =  [        
-                        { id: 4, name: "kevin", age: "18", marks: 75 },        
-                        { id: 5, name: "lisa", age: "20", marks: 95 },        
-                        { id: 6, name: "matt", age: "19", marks: 60 }    
-                    ];
-    arr = arr.concat(newArray);
-    console.log(arr);
+function concatenateArray() {
+  //Write your code here, just console.log
+  arr.push(
+    {id:arr.length+1, name:"samantha", age:"21", marks:90},
+    {id:arr.length+2, name:"kevin", age:"19", marks:70},
+    {id:arr.length+3, name:"emily", age:"20", marks:55}
+  );
+  console.log(arr);
 }
